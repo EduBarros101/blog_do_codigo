57d6898f286577aa77541db1d007e0a44a44ef4c
Eduardo Barros <edu.barros101@gmail.com>
2023-05-29 11:55:48 -0300

    Proteção de senha implementada.

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('db.sqlite');
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('db.sqlite')
 
 const POSTS_SCHEMA = `
   CREATE TABLE IF NOT EXISTS posts (
@@ -7,32 +7,32 @@ const POSTS_SCHEMA = `
     titulo VARCHAR(50) NOT NULL,
     conteudo VARCHAR(140)
   )
-  `;
+  `
 
 const USUARIOS_SCHEMA = `
   CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(40) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    senhaHash VARCHAR(255) NOT NULL
   )
-  `;
+  `
 
 db.serialize(() => {
-  db.run('PRAGMA foreign_keys=ON');
-  db.run(POSTS_SCHEMA);
-  db.run(USUARIOS_SCHEMA);
+  db.run('PRAGMA foreign_keys=ON')
+  db.run(POSTS_SCHEMA)
+  db.run(USUARIOS_SCHEMA)
 
   db.each('SELECT * FROM usuarios', (err, usuario) => {
-    console.log('Usuarios: ');
-    console.log(usuario);
-  });
-});
+    console.log('Usuarios: ')
+    console.log(usuario)
+  })
+})
 
 process.on('SIGINT', () =>
   db.close(() => {
-    process.exit(0);
+    process.exit(0)
   })
-);
+)
 
-module.exports = db;
+module.exports = db
--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,43 +1,44 @@
-const Usuario = require('./usuarios-modelo');
-const { InvalidArgumentError, InternalServerError } = require('../erros');
+const Usuario = require('./usuarios-modelo')
+const { InvalidArgumentError, InternalServerError } = require('../erros')
 
 module.exports = {
   adiciona: async (req, res) => {
-    const { nome, email, senha } = req.body;
+    const { nome, email, senha } = req.body
 
     try {
       const usuario = new Usuario({
         nome,
         email,
-        senha
-      });
+      })
 
-      await usuario.adiciona();
+      await usuario.adicionaSenha(senha)
 
-      res.status(201).json();
+      await usuario.adiciona()
+
+      res.status(201).json()
     } catch (erro) {
       if (erro instanceof InvalidArgumentError) {
-        res.status(422).json({ erro: erro.message });
+        res.status(422).json({ erro: erro.message })
       } else if (erro instanceof InternalServerError) {
-        res.status(500).json({ erro: erro.message });
+        res.status(500).json({ erro: erro.message })
       } else {
-        res.status(500).json({ erro: erro.message });
+        res.status(500).json({ erro: erro.message })
       }
     }
   },
 
   lista: async (req, res) => {
-    const usuarios = await Usuario.lista();
-    res.json(usuarios);
+    const usuarios = await Usuario.lista()
+    res.json(usuarios)
   },
 
   deleta: async (req, res) => {
-    const usuario = await Usuario.buscaPorId(req.params.id);
+    const usuario = await Usuario.buscaPorId(req.params.id)
     try {
-      await usuario.deleta();
-      res.status(200).send();
+      await usuario.deleta()
+      res.status(200).send()
     } catch (erro) {
-      res.status(500).json({ erro: erro });
+      res.status(500).json({ erro: erro })
     }
-  }
-};
+  },
+}
