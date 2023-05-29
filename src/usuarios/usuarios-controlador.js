f1ecdcf155359c8ae21a279612eea9fa5e9aeddd
Eduardo Barros <edu.barros101@gmail.com>
2023-05-29 16:13:55 -0300

    Incluindo geração de tokens.

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -1,5 +1,13 @@
 const Usuario = require('./usuarios-modelo')
 const { InvalidArgumentError, InternalServerError } = require('../erros')
+const jwt = require('jsonwebtoken')
+
+function criaTokenJWT(usuario) {
+  const payload = { id: usuario.id }
+  const token = jwt.sign(payload, 'senha-secreta')
+
+  return token
+}
 
 module.exports = {
   adiciona: async (req, res) => {
@@ -28,6 +36,9 @@ module.exports = {
   },
 
   login: (req, res) => {
+    const token = criaTokenJWT(req.user)
+    res.set('Authorization', token)
+
     res.status(204).send()
   },
 
